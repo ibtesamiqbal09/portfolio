e606fe79e6d9637f8793fb2a91f34f0feb930e22
Muhammad Ibtesam <ibtesamiqbal09@gmail.com>
2025-08-22 17:46:22 +0500

    Updated project files

--- vite.config.ts
+++ vite.config.ts
@@ -6,28 +6,19 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 const isProduction = process.env.NODE_ENV === "production";
-const isReplit = process.env.REPL_ID !== undefined;
 
 export default defineConfig({
   plugins: [
     react(),
     ...(!isProduction ? [runtimeErrorOverlay()] : []),
-    ...(!isProduction && isReplit
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
+      "@": path.resolve(__dirname, "src"),
       "@shared": path.resolve(__dirname, "shared"),
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(__dirname, "client"),
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
